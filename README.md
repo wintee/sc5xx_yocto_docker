fd7770ea2dc2c0f3651513659a38c5c46f63534a
David Gibson <david.gibson@analog.com>
2020-05-21 09:00:11 +0100

    Revised script to take a build target and bitbake commands allowing for any ADI target to be built

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sc5xx_yocto_docker
 Docker image for building ADSP-SC5xx Yocto Linux
+
+## Building the image
+To build the image:
+  docker build --tag <tagname> .
+
+To run the image and build the Linux components:
+  docker run -v `pwd`/docker_artefacts:/linux/build/tmp/deploy/images <tagname> ./buildOnDocker.sh -m <machine> <bitbake args>
+
+  Where machine is one of sc589-sam sc589-ezkit sc584-ezkit sc573-ezkit and bitbake args are the bitbake commands to execute
--- buildOnDocker.sh
+++ buildOnDocker.sh
@@ -1,14 +1,46 @@
-:
+#!/bin/bash
 # Script for automatically building Yocto Linux for ADSP-SC5xx processors
 # Copyright David Gibson <wintee@gmail.com> All Rights Reserved.
 # This script is distributed under the standard MIT Licnse.
 # Configure these for your build
 
-export DEV_BRANCH=develop/yocto-1.0.0
-export GIT_EMAIL="you@dev.null"
-export GIT_NAME="Fill in your name and e-mail"
-export ARCH=sc589
-export BOARD=mini
+DEV_BRANCH=develop/yocto-1.0.0
+GIT_EMAIL="you@dev.null"
+GIT_NAME="Fill in your name and e-mail"
+SCRIPT_TARGET=""
+BUILD_ARGS=""
+
+function usage() {
+    echo "$0: -m <machine> <bitbake commands>"
+    echo "     where machine is one of:"
+    echo "          sc589-mini"
+    echo "          sc589-ezkit"
+    echo "          sc584-ezkit"
+    echo "          sc573-ezkit"
+    echo ""
+    echo "Bitbake commands are usually the commands to build ADI images:"
+    echo "          adsp-sc5xx-full"
+    echo "          adsp-sc5xx-ramdisk"
+    echo "          u-boot-adi"
+    echo "          linux-adi"
+    echo "  Please refer to wiki.analog.com/resources/tools-software/linuxdsp for more details"
+    exit $1
+}
+# Parse default args
+if [ $# -lt 3 ]
+then
+  echo "Error: Please provide correct arguments to the script"
+  usage -1
+fi
+if [ "$1" == "-m" ]
+then
+  SCRIPT_TARGET=$2
+else
+  echo "Please provide a valid machine"
+  usage -1
+fi
+shift 2
+BUILD_ARGS="$*"
 
 # Configure these, not so much
 #  CCES 2.8.3 is a known good version for Linux, don't update it
@@ -33,12 +65,13 @@ export LANG=${SET_LANG}
 # Install CCES, needed for open source tools
 if [ ! -d /opt/analog/cces/${CCES_VERSION} ]
 then
-    echo "Installing CrossCore Embedded Studio ${CCES_VERSION}"
     CCESFILE=adi-CrossCoreEmbeddedStudio-linux-x86-${CCES_VERSION}.deb
     if [ ! -e ${CCESFILE} ]
     then
-        wget http://download.analog.com/tools/CrossCoreEmbeddedStudio/Releases/Release_${CCES_VERSION}/${CCESFILE}
-    fi
+        echo "Downloading CrossCore Embedded Studio ${CCES_VERSION}"
+        wget -q http://download.analog.com/tools/CrossCoreEmbeddedStudio/Releases/Release_${CCES_VERSION}/${CCESFILE}
+    fi 
+    echo "Installing CrossCore Embedded Studio ${CCES_VERSION}"
     export DEBIAN_FRONTEND=noninteractive
     echo "adi-cces-${CCES_VERSION} adi-cces-${CCES_VERSION}/run-ocd-config boolean true" | debconf-set-selections
     echo "adi-cces-${CCES_VERSION} adi-cces-${CCES_VERSION}/accept-sla boolean true" | debconf-set-selections    
@@ -71,9 +104,6 @@ ${WD}/bin/repo sync
 cat sources/poky/meta/conf/sanity.conf | sed -e 's/^INHERIT/# INHERIT/' > sources/poky/meta/conf/sanity.conf
 
 # Set up environment to build
-source ./setup-environment -m adsp-${ARCH}-${BOARD}
+source ./setup-environment -m adsp-${SCRIPT_TARGET}
 
-bitbake adsp-sc5xx-full
-bitbake adsp-sc5xx-ramboot
-
-
+bitbake ${BUILD_ARGS}
